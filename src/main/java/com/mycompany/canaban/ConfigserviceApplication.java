392e10da3c694a8368d58402e8c201062f1deb27
antongusev <gucebanton@gmal.com>
2017-02-14 23:35:03 +0300

    initial commit

--- src/main/java/com/mycompany/canaban/ConfigserviceApplication.java
+++ src/main/java/com/mycompany/canaban/ConfigserviceApplication.java
@@ -3,12 +3,10 @@ package com.mycompany.canaban;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableAutoConfiguration
-@EnableEurekaServer
-@EnableConfigServer
 @EnableAdminServer
 public class ConfigserviceApplication {
 
