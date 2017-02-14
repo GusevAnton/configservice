745faf927c1e157395e002e87870e2802c41e897
antongusev <gucebanton@gmal.com>
2017-02-14 23:21:09 +0300

    admin server added

--- src/main/java/com/mycompany/canaban/ConfigserviceApplication.java
+++ src/main/java/com/mycompany/canaban/ConfigserviceApplication.java
@@ -1,5 +1,6 @@
 package com.mycompany.canaban;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableAutoConfiguration
 @EnableEurekaServer
 @EnableConfigServer
+@EnableAdminServer
 public class ConfigserviceApplication {
 
 	public static void main(String[] args) {
